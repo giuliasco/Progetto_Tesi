da7925439b15ac7a20fcb226dc105e58cbb5e8db
giuliasco <giulia.scoccia@gmail.com>
2020-05-04 15:37:50 +0200

    first commit

---
+++ CC2/cmake-build-debug/CMakeFiles/CC2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CC2"
+  "CC2.pdb"
+  "CMakeFiles/CC2.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CC2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
