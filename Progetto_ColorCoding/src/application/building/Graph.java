00b5fd38a2f124ba788ee400151196fd5cfa0df6
giuliasco <giulia.scoccia@gmail.com>
2020-05-22 19:14:19 +0200

    unione di treelet

--- Progetto_ColorCoding/src/application/building/Graph.java
+++ Progetto_ColorCoding/src/application/building/Graph.java
@@ -42,4 +42,32 @@ public class Graph {
 		return c;
 	}
 	//vedere se può essere utile un metodo che mi aggiunge un vertice
+
+	public void treeletFromGraph(Graph g,int k){
+		int occ=0;
+		Vector<HashMap<Treelet, Integer>> vector = new Vector<HashMap<Treelet, Integer>>();
+		int color[] =g.colorGraph(k);
+		//per ogni nodo creiamo l'albero composto dal solo nodo con occorrenza 1;
+		for ( int v=0 ; v<g.V ; v++ ){
+			ColorNode c = new ColorNode (v,color[v]);
+			Treelet t = new Treelet(c);
+			HashMap<Treelet,Integer> opt = new HashMap<Treelet, Integer>();
+			opt.put(t,occ+1);
+			vector.add(opt);
+		}
+		for(int h=2; h==k ; h++){
+			for ( int u=0; u<g.adj.size() ;u++){
+				for(int v = 0 ; v < g.adj.get(u).size() ; v++){
+
+				}
+			}
+		}
+
+	}
 }
+
+/*
+devo capire se c'è un modo che mi permette facilmente dalla numero del treelet di risalire al treelet stesso???? questo mi serv per i merge
+però salvarmi il treelet nell'haash map ad ora non è funzionale perchè dovrei incasinare una frega le cose per occ.
+DA CAPIRE
+ */
